c3b03a9f6d6f38e5f491175fec8d85d30811681a
NabilaNHossain <nabilanhossain@gmail.com>
2022-03-22 19:09:29 -0500

    homeworkk16

---
+++ src/xpathHomework/HW_16_03172022_send_keys2.java
@@ -0,0 +1,40 @@
+package xpathHomework;
+
+import java.sql.Driver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class HW_16_03172022_send_keys2 {
+	WebDriver driver;
+
+	@BeforeTest
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\tthik\\eclipse-workspace\\SeleniumMethods\\driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.uhc.com/");
+	}
+
+	@Test(priority = 1)
+	public void sendKeysUhc() throws InterruptedException {
+		driver.findElement(By.xpath("//input[@id='search-desktop']")).sendKeys("Insurance");
+	}
+
+	@Test(priority = 2)
+	public void sendKeysAndClick() {
+		driver.findElement(By.id("search__button-desktop")).click();
+	}
+
+	@AfterTest
+	public void tearUp() {
+		driver.quit();
+	}
+}
---
+++ src/xpathHomework/HW_16_03172022_send_keys_clear.java
@@ -0,0 +1,40 @@
+package xpathHomework;
+
+import java.sql.Driver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class HW_16_03172022_send_keys_clear {
+	WebDriver driver;
+
+	@BeforeTest
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\tthik\\eclipse-workspace\\SeleniumMethods\\driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.ebay.com/");
+	}
+
+	@Test
+	public void sendKeysEbay()  {
+		driver.findElement(By.xpath("//input[@id='gh-ac']")).sendKeys("handbag");
+		driver.findElement(By.xpath("//input[@id='gh-btn']")).click();
+		driver.findElement(By.xpath("//input[@id='gh-ac']")).clear();
+		driver.findElement(By.xpath("//input[@id='gh-ac']")).sendKeys("laptop");
+		driver.findElement(By.xpath("//input[@id='gh-btn']")).click();
+		System.out.println("The current URL is : " + driver.getCurrentUrl());
+		}
+
+	@AfterTest
+	public void tearUp() {
+		driver.quit();
+	}
+}
